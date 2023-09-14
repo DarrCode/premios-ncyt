6ebce346e26f5a402a77ee28961357e05680814c
Diego <diego.rojas0640@gmail.com>
2023-09-14 15:32:14 -0400

    first commit

---
+++ src/components/auth/FormLogin.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+      <h1 class="my-7">Inicio de sesión</h1>
+      <form >
+        <v-text-field
+          v-model="form.email"
+          type="email"
+          color="primary"
+          outlined
+          hide-details="auto"
+          label="Dirección de correo"
+          class="my-10"
+          clearable
+        ></v-text-field>
+        <v-text-field
+          v-model="form.password"
+          color="primary"
+          :rules="[rules.required, rules.min]"
+          type="password"
+          label="Contraseña"
+          hint="Al menos 8 caracteres"
+          counter
+          class="mb-10"
+          outlined
+        ></v-text-field>
+  
+        <v-btn
+          block
+          color="primary"
+          elevation="3"
+          large
+          @click="login"
+        >
+          Iniciar sesión
+        </v-btn>
+        <a href="" class="text-decoration-none">
+          <p class="text-right mt-5 text-blue-darken-1">¿Olvidaste tu contraseña?</p>
+        </a>
+      </form>
+    </div>
+  </template>
+  <script>
+  
+  export default {
+    name: 'FormLogin',
+    data () {
+      return {
+        form: {
+          email: '',
+          password: '',
+        },
+        rules: {
+          required: value => !!value || 'Requerido.',
+          min: v => v.length >= 8 || 'Minimo 8 caracteres',
+          emailMatch: () => (`El correo y la contrasena no coinsiden`),
+        },
+      }
+    },
+    methods: {
+      login (){
+        this.$router.push('dashboard')
+      }
+    }
+  }
+  </script>
---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="premios-auth">
+    <v-container fluid class="px-0 py-0">
+      <v-row class="no-gutters min-vh-100">
+        <v-col cols="12" sm="5">
+          <div class="hero-wrap d-flex align-items-center h-100">
+            <div class="hero-mask wrapper"></div>
+            <div class="hero-content mx-auto w-100 h-100">
+              <v-container class="d-flex flex-column h-100">
+                <v-row class="no-gutters">
+                  <v-col cols="11" lg="9" class="mx-auto">
+                    <div class="logo mt-5 ">
+                      <a class="d-flex white--text" href="index.html" title="premios">
+                        LOGO
+                      </a>
+                    </div>
+                  </v-col>
+                </v-row>
+                <v-row class="no-gutters my-auto">
+                  <v-col cols="12" lg="10" class="mx-auto">
+                    <h1 class="text-9 font-weight-600 mb-5 white--text">
+                      Sistema para la administración de los Premios Nacionales de Ciencia y Tecnología
+                    </h1>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </div>
+          </div>
+        </v-col>
+        <v-col cols="12" sm="7">
+          <v-container class="pa-10">
+            <v-row align="center" no-gutters>
+              <v-col cols="12" lg="9" class="mx-auto mt-5">
+                <FormLogin v-if="$route.name == 'login'"/>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AuthView',
+	mounted (){
+		console.log(this.$route);
+	},
+  components: {
+    FormLogin: () => import(/* webpackChunkName: "about" */ '@/components/auth/FormLogin.vue'),
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+<style>
+.premios-auth .hero-wrap {
+  position: relative;
+  overflow: hidden;
+}
+
+.premios-login-brand {
+  width: 180px;
+}
+
+.hand-emoji {
+  width: 30px;
+}
+.min-vh-100 {
+  min-height: 100vh !important;
+}
+.h-100 {
+  height: 100%!important;
+}
+
+.premios-auth .hero-wrap .hero-mask{
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+}
+
+.premios-auth .hero-wrap .hero-mask {
+  z-index: 1;
+}
+
+.premios-auth .hero-wrap .hero-content {
+  position: relative;
+  z-index: 2;
+}
+
+.premios-auth .hero-wrap .hero-bg {
+  z-index: 0;
+  background-attachment: fixed;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  transition: background-image 300ms ease-in 200ms;
+}
+
+.wrapper {
+  height: 100%;
+  width: 100%;
+  left:0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  position: absolute;
+  background: linear-gradient(124deg, #3861f4, #3861f4, #1ddde8, #3861f4, #3861f4);
+  background-size: 1800% 1800%;
+
+  -webkit-animation: rainbow 18s ease infinite;
+  -z-animation: rainbow 18s ease infinite;
+  -o-animation: rainbow 18s ease infinite;
+  animation: rainbow 18s ease infinite;
+}
+
+@-webkit-keyframes rainbow {
+  0%{background-position:0% 82%}
+  50%{background-position:100% 19%}
+  100%{background-position:0% 82%}
+}
+@-moz-keyframes rainbow {
+  0%{background-position:0% 82%}
+  50%{background-position:100% 19%}
+  100%{background-position:0% 82%}
+}
+@-o-keyframes rainbow {
+  0%{background-position:0% 82%}
+  50%{background-position:100% 19%}
+  100%{background-position:0% 82%}
+}
+@keyframes rainbow { 
+  0%{background-position:0% 82%}
+  50%{background-position:100% 19%}
+  100%{background-position:0% 82%}
+}
+
+</style>
