ea6ffdcd67eda4539dffec03b8dcbfd99b4569a6
alexjrf03 <alexjrf27@gmail.com>
2023-10-20 15:41:49 -0400

    checkbox completed

--- src/components/postulates/cardsPostulateData/FilesData.vue
+++ src/components/postulates/cardsPostulateData/FilesData.vue
@@ -13,7 +13,60 @@
         <v-list-item-content>
           <v-list-item-title> {{ files.cartaPostulacion ? 'Carta de Postulación' : '' }}</v-list-item-title>
         </v-list-item-content>
+        <v-list-item-action v-if="files.cedula">
+            <v-checkbox></v-checkbox>
+        </v-list-item-action>
         <v-list-item-action v-if="files.cartaPostulacion">
+          <v-btn
+            elevation="3"
+            fab
+            small
+            color="primary"
+            class="ml-5"
+            @click="fileDownload(files.cartaPostulacion, 'Carta de Postulacion')"
+          >
+            <v-icon style="color:white">
+              mdi-eye
+            </v-icon>
+          </v-btn>
+        </v-list-item-action>
+        
+      </v-list-item>
+
+      <v-divider v-if="files.cartaPostulacion"></v-divider>
+
+      <v-list-item v-if="files.cedula">
+        <v-list-item-content>
+          <v-list-item-title> {{ files.cedula ? 'Cédula de Identidad' : '' }}</v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-action v-if="files.cedula">
+            <v-checkbox></v-checkbox>
+        </v-list-item-action>
+        <v-list-item-action v-if="files.cedula">
+            <v-btn
+              elevation="3"
+              fab
+              small
+              color="primary"
+              @click="fileDownload(files.cartaPostulacion)"
+            >
+              <v-icon style="color:white">
+                mdi-eye
+              </v-icon>
+            </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+
+      <v-divider v-if="files.cedula"></v-divider>
+
+      <v-list-item v-if="menciones.curriculum">
+        <v-list-item-content>
+          <v-list-item-title> {{ menciones.curriculum ? 'Curriculum' : '' }}</v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-action v-if="menciones.curriculum">
+            <v-checkbox></v-checkbox>
+        </v-list-item-action>
+        <v-list-item-action v-if="menciones.curriculum">
           <v-btn
             elevation="3"
             fab
@@ -26,59 +79,24 @@
             </v-icon>
           </v-btn>
         </v-list-item-action>
-        
       </v-list-item>
-      <v-divider v-if="files.cartaPostulacion"></v-divider>
 
-      <v-list-item v-if="files.cedula">
-        <v-list-item-content>
-          <v-list-item-title> {{ files.cedula ? 'Cédula de Identidad' : '' }}</v-list-item-title>
-        </v-list-item-content>
-        <v-list-item-action v-if="files.cedula">
-          <v-btn
-            elevation="3"
-            fab
-            small
-            color="primary"
-            @click="fileDownload(files.cedula, 'Cedula de Identidad')"
-          >
-            <v-icon style="color:white">
-              mdi-eye
-            </v-icon>
-          </v-btn>
-        </v-list-item-action>
-      </v-list-item>
-      <v-divider v-if="files.cedula"></v-divider>
-      <v-list-item v-if="menciones.curriculum">
-        <v-list-item-content>
-          <v-list-item-title> {{ menciones.curriculum ? 'Curriculum' : '' }}</v-list-item-title>
-        </v-list-item-content>
-        <v-list-item-action v-if="menciones.curriculum">
-          <v-btn
-            elevation="3"
-            fab
-            small
-            color="primary"
-            @click="fileDownload(menciones.curriculum, 'Curriculum')"
-          >
-            <v-icon style="color:white">
-              mdi-eye
-            </v-icon>
-          </v-btn>
-        </v-list-item-action>
-      </v-list-item>
       <v-divider v-if="menciones.curriculum"></v-divider>
+
       <v-list-item v-if="menciones.listaPostulados">
         <v-list-item-content>
           <v-list-item-title> {{ menciones.listaPostulados ? 'Lista de postulados' : '' }}</v-list-item-title>
         </v-list-item-content>
+        <v-list-item-action v-if="menciones.listaPostulados">
+          <v-checkbox></v-checkbox>
+        </v-list-item-action>
         <v-list-item-action v-if="menciones.listaPostulados">
           <v-btn
             elevation="3"
             fab
             small
             color="primary"
-            @click="fileDownload(menciones.listaPructos, 'Lista de postulados')"
+            @click="fileDownload(files.cartaPostulacion, 'Carta de Postulacion')"
           >
             <v-icon style="color:white">
               mdi-eye
@@ -86,154 +104,192 @@
           </v-btn>
         </v-list-item-action>
       </v-list-item>
+
       <v-list-item v-if="menciones.listaPructos">
         <v-list-item-content>
           <v-list-item-title> {{ menciones.listaPructos ? 'Lista de productos' : '' }}</v-list-item-title>
         </v-list-item-content>
         <v-list-item-action v-if="menciones.listaPructos">
-          <v-btn
-            elevation="3"
-            fab
-            small
-            color="primary"
-            @click="fileDownload(menciones.listaPructos, 'Lista de productos')"
-          >
-            <v-icon style="color:white">
-              mdi-eye
-            </v-icon>
-          </v-btn>
+          <v-checkbox></v-checkbox>
         </v-list-item-action>
-      </v-list-item>
-      <v-divider v-if="menciones.listaPructos"></v-divider>
-      <v-list-item v-if="menciones.titulo">
-        <v-list-item-content>
-          <v-list-item-title> {{ menciones.titulo ? 'Titulo' : '' }}</v-list-item-title>
-        </v-list-item-content>
-        <v-list-item-action v-if="menciones.titulo">
-          <v-btn
-            elevation="3"
-            fab
-            small
-            color="primary"
-            @click="fileDownload(menciones.titulo, 'Titulo')"
-          >
-            <v-icon style="color:white">
-              mdi-eye
-            </v-icon>
-          </v-btn>
-        </v-list-item-action>
-      </v-list-item>
-      <v-divider v-if="menciones.titulo"></v-divider>
-      <v-list-item v-if="menciones.PresentacionDescriptiva">
-        <v-list-item-content>
-          <v-list-item-title> {{ menciones.PresentacionDescriptiva ? 'Presentación descriptiva' : ''}}</v-list-item-title>
-        </v-list-item-content>
-        <v-list-item-action v-if="menciones.PresentacionDescriptiva">
-          <v-btn
-            elevation="3"
-            fab
-            small
-            color="primary"
-            @click="fileDownload(menciones.PresentacionDescriptiva, 'Presentacion descriptiva')"
-          >
-            <v-icon style="color:white">
-              mdi-eye
-            </v-icon>
-          </v-btn>
-        </v-list-item-action>
-        </v-list-item>
-        <v-divider v-if="menciones.PresentacionDescriptiva"></v-divider>
-        <v-list-item v-if="menciones.copiaTrabajo">
-        <v-list-item-content>
-          <v-list-item-title> {{ menciones.copiaTrabajo ? 'Copia de Trabajo' : ''}}</v-list-item-title>
-        </v-list-item-content>
-        <v-list-item-action v-if="menciones.copiaTrabajo">
-          <v-btn
-            elevation="3"
-            fab
-            small
-            color="primary"
-            @click="fileDownload(menciones.copiaTrabajo, 'Copia de Trabajo')"
-          >
-            <v-icon style="color:white">
-              mdi-eye
-            </v-icon>
-          </v-btn>
-          </v-list-item-action>
-        </v-list-item>
-        <v-divider v-if="menciones.copiaTrabajo"></v-divider>
-        <v-list-item v-if="menciones.documentExel">
-          <v-list-item-content>
-            <v-list-item-title> {{ menciones.documentExel ? 'Documento Excel' : ''}}</v-list-item-title>
-          </v-list-item-content>
-          <v-list-item-action v-if="menciones.documentExel">
+        <v-list-item-action v-if="menciones.listaPructos">
             <v-btn
               elevation="3"
               fab
               small
               color="primary"
-              @click="fileDownload(menciones.documentExel, 'Documento Excel')"
+              @click="fileDownload(files.cartaPostulacion, 'Carta de Postulacion')"
             >
               <v-icon style="color:white">
                 mdi-eye
               </v-icon>
             </v-btn>
-          </v-list-item-action>
-        </v-list-item>
+        </v-list-item-action>
+      </v-list-item>
+
+      <v-divider v-if="menciones.listaPructos"></v-divider>
+
+      <v-list-item v-if="menciones.titulo">
+        <v-list-item-content>
+          <v-list-item-title> {{ menciones.titulo ? 'Titulo' : '' }}</v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-action v-if="menciones.titulo">
+            <v-checkbox></v-checkbox>
+        </v-list-item-action>
+        <v-list-item-action v-if="menciones.titulo">
+            <v-btn
+              elevation="3"
+              fab
+              small
+              color="primary"
+              @click="fileDownload(files.cartaPostulacion, 'Carta de Postulacion')"
+            >
+              <v-icon style="color:white">
+                mdi-eye
+              </v-icon>
+            </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+
+      <v-divider v-if="menciones.titulo"></v-divider>
+
+      <v-list-item v-if="menciones.PresentacionDescriptiva">
+        <v-list-item-content>
+          <v-list-item-title> {{ menciones.PresentacionDescriptiva ? 'Presentación descriptiva' : ''}}</v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-action v-if="menciones.PresentacionDescriptiva">
+            <v-checkbox></v-checkbox>
+        </v-list-item-action>
+        <v-list-item-action v-if="menciones.PresentacionDescriptiva">
+            <v-btn
+              elevation="3"
+              fab
+              small
+              color="primary"
+              @click="fileDownload(files.cartaPostulacion, 'Carta de Postulacion')"
+            >
+              <v-icon style="color:white">
+                mdi-eye
+              </v-icon>
+            </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+
+      <v-divider v-if="menciones.PresentacionDescriptiva"></v-divider>
+
+      <v-list-item v-if="menciones.copiaTrabajo">
+      <v-list-item-content>
+        <v-list-item-title> {{ menciones.copiaTrabajo ? 'Copia de Trabajo' : ''}}</v-list-item-title>
+      </v-list-item-content>
+      <v-list-item-action v-if="menciones.copiaTrabajo">
+          <v-checkbox></v-checkbox>
+        </v-list-item-action>
+      <v-list-item-action v-if="menciones.copiaTrabajo">
+          <v-btn
+            elevation="3"
+            fab
+            small
+            color="primary"
+            @click="fileDownload(files.cartaPostulacion, 'Carta de Postulacion')"
+          >
+            <v-icon style="color:white">
+              mdi-eye
+            </v-icon>
+          </v-btn>
+      </v-list-item-action>
+      </v-list-item>
+
+      <v-divider v-if="menciones.copiaTrabajo"></v-divider>
+
+      <v-list-item v-if="menciones.documentExel">
+        <v-list-item-content>
+          <v-list-item-title> {{ menciones.documentExel ? 'Documento Excel' : ''}}</v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-action v-if="menciones.documentExel">
+          <v-checkbox></v-checkbox>
+        </v-list-item-action>
+        <v-list-item-action v-if="menciones.documentExel">
+          <v-btn
+            elevation="3"
+            fab
+            small
+            color="primary"
+            @click="fileDownload(files.cartaPostulacion, 'Carta de Postulacion')"
+          >
+            <v-icon style="color:white">
+              mdi-eye
+            </v-icon>
+          </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+
       <v-divider v-if="menciones.documentExel"></v-divider>
+
       <v-list-item v-if="menciones.videoDemostrativo">
         <v-list-item-content>
           <v-list-item-title> {{ menciones.videoDemostrativo ? 'Video demostrativo' : ''}}</v-list-item-title>
         </v-list-item-content>
         <v-list-item-action v-if="menciones.videoDemostrativo">
-          <v-btn
-            elevation="3"
-            fab
-            small
-            color="primary"
-            @click="fileDownload(menciones.videoDemostrativo, 'Video demostrativo')"
-          >
-            <v-icon style="color:white">
-              mdi-eye
-            </v-icon>
-          </v-btn>
+          <v-checkbox></v-checkbox>
+        </v-list-item-action>
+        <v-list-item-action v-if="menciones.videoDemostrativo">
+            <v-btn
+              elevation="3"
+              fab
+              small
+              color="primary"
+              @click="fileDownload(files.cartaPostulacion, 'Carta de Postulacion')"
+            >
+              <v-icon style="color:white">
+                mdi-eye
+              </v-icon>
+            </v-btn>
         </v-list-item-action>
       </v-list-item>
+
       <v-divider v-if="menciones.videoDemostrativo"></v-divider>
+
       <v-list-item v-if="menciones.cartaAutorizacion">
         <v-list-item-content>
           <v-list-item-title> {{ menciones.cartaAutorizacion ? 'Carta de autorización' : ''}}</v-list-item-title>
         </v-list-item-content>
         <v-list-item-action v-if="menciones.cartaAutorizacion">
-          <v-btn
-            elevation="3"
-            fab
-            small
-            color="primary"
-            @click="fileDownload(menciones.cartaAutorizacion, 'Carta de autorizacion')"
-          >
-            <v-icon style="color:white">
-              mdi-eye
-            </v-icon>
-          </v-btn>
+          <v-checkbox></v-checkbox>
+        </v-list-item-action>
+        <v-list-item-action v-if="menciones.cartaAutorizacion">
+            <v-btn
+              elevation="3"
+              fab
+              small
+              color="primary"
+              @click="fileDownload(files.cartaPostulacion, 'Carta de Postulacion')"
+            >
+              <v-icon style="color:white">
+                mdi-eye
+              </v-icon>
+            </v-btn>
         </v-list-item-action>
       </v-list-item>
+
       <v-list-item v-if="menciones.documentoAcreditable">
         <v-list-item-content>
           <v-list-item-title> {{ menciones.documentoAcreditable ? 'Documento Acreditable' : ''}}</v-list-item-title>
         </v-list-item-content>
         <v-list-item-action v-if="menciones.documentoAcreditable">
-          <v-btn
-            elevation="3"
-            fab
-            small
-            color="primary"
-            @click="fileDownload(menciones.documentoAcreditable, 'Documento Acreditable')"
-          >
-            <v-icon style="color:white">
-              mdi-eye
-            </v-icon>
-          </v-btn>
+          <v-checkbox></v-checkbox>
+        </v-list-item-action>
+        <v-list-item-action v-if="menciones.documentoAcreditable">
+            <v-btn
+              elevation="3"
+              fab
+              small
+              color="primary"
+              @click="fileDownload(files.cartaPostulacion, 'Carta de Postulacion')"
+            >
+              <v-icon style="color:white">
+                mdi-eye
+              </v-icon>
+            </v-btn>
         </v-list-item-action>
       </v-list-item>
     </v-list>
@@ -304,3 +360,10 @@
     }
   }
 </script>
+
+<style>
+  .myCheckbox{
+    margin: 8px 8px 0px 0px !important;
+  }
+
+</style>
